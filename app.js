74742ff40ab58d286452e72500dd9435c1a3b100
Sunil Saini <72140848+suniljmaldiya@users.noreply.github.com>
2026-02-10 16:48:57 +0530

    Update and rename Gruntfile.js to app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("App Runner Node.js is working 🚀");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
